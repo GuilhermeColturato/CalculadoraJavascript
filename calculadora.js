216107e3cfee7f627154b0dc92f7176deeb1b42c
GuilhermeColturato <guilhermecolturato13@gmail.com>
2020-10-02 21:28:34 -0300

    Adicionei os arquivos referentes a calculadora Javascript

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+var valor1 = prompt("Digite o primeiro valor da soma:");
+var valor2 = prompt("Digite o segundo valor da soma:");
+
+var v1 = parseInt(valor1);
+var v2 = parseInt(valor2);
+
+var resultado = v1 + v2;
+
+console.log(resultado);
+
+alert("Resultado da soma eh:"  +resultado);
